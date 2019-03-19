57d4a1ae01b37cc15ea4dd76803a2683942ad9c0
Anton Mendelson <1choice@mail.ru>
2019-03-19 19:33:17 +0300

    creating test layout

---
+++ precache-manifest.420d192a13d49c745eb84eab6c3eef50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "15e1b9c5b0c9333ec8ea",
+    "url": "/static/js/main.bb6402b5.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "15e1b9c5b0c9333ec8ea",
+    "url": "/static/css/main.42912525.chunk.css"
+  },
+  {
+    "revision": "822f59f1122cd35899712c2ffad9d94f",
+    "url": "/index.html"
+  }
+];
